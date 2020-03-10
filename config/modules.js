4aecf071f8ec61fe26699b63e6e9e51334cc863a
Santosh Bhandari <bsantosh909@gmail.com>
2020-03-10 15:52:23 +0545

    [FIX] Sitemap fix attempt

--- config/modules.js
+++ config/modules.js
@@ -1,6 +1,3 @@
-import { scan } from 'fs-nextra';
-import { basename, extname } from 'path';
-
 export default {
 	modules: [
 		'bootstrap-vue/nuxt',
@@ -15,16 +12,6 @@ export default {
 		cacheTime: 1000 * 60 * 60 * 2,
 		trailingSlash: true,
 		gzip: true,
-		hostname: 'https://blog.santoshb.com.np',
-		routes: async () => {
-			const articles = await scan('contents/articles', { filter: (stats, path) => stats.isFile() && extname(path) === '.md' });
-			const sitemapList = [...articles.keys()].map((file) => ({
-				url: `/articles/${basename(file, '.md')}`,
-				priority: 1,
-				lastmod: articles.get(file).mtime
-			}));
-			// Returning the list...
-			return [].concat(sitemapList);
-		}
+		hostname: 'https://blog.santoshb.com.np'
 	}
 }
