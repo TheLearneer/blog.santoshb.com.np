b4c2c2b9145f9ea1513b4d33ec54b64b6fb01ec1
Santosh Bhandari <bsantosh909@gmail.com>
2020-03-10 15:45:52 +0545

    Sitemap

--- config/modules.js
+++ config/modules.js
@@ -1,9 +1,29 @@
+import { scan } from 'fs-nextra';
+import { basename, extname } from 'path';
+
 export default {
 	modules: [
-		'bootstrap-vue/nuxt'
+		'bootstrap-vue/nuxt',
+		'@nuxtjs/sitemap'
 	],
 	// Bootstrap-vue configuration
 	bootstrapVue: {
 		icons: true
+	},
+	// Sitemap configuration
+	sitemap: {
+		cacheTime: 1000 * 60 * 60 * 2,
+		trailingSlash: true,
+		gzip: true,
+		routes: async () => {
+			const articles = await scan('contents/articles', { filter: (stats, path) => stats.isFile() && extname(path) === '.md' });
+			const sitemapList = [...articles.keys()].map((file) => ({
+				url: `/articles/${basename(file, '.md')}`,
+				priority: 1,
+				lastmod: articles.get(file).mtime
+			}));
+			// Returning the list...
+			return [].concat(sitemapList);
+		}
 	}
 }
